bad810a2ebcf2f12b918b1694b206ff46fd53016
Mehedi rabby <mehedi15-2995@diu.edu.bd>
2022-10-19 16:38:50 +0600

    pass added

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -5,7 +5,16 @@ const Login = () => {
   return (
     <div className="form-container">
       <h2 className="from-title">Login</h2>
-      <form></form>
+      <form>
+        <div className="from-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" required />
+        </div>
+        <div className="from-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" required />
+        </div>
+      </form>
     </div>
   );
 };
